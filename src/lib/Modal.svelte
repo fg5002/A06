bc4f94c2d09be53b223cf055bd7f03e7df25323e
Attila <attila.ocsag@gmail.com>
2024-02-01 11:45:14 +0100

    modal height reduced to 50%

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -17,7 +17,7 @@
 
 {#if showModal}
   <div class="fixed inset-0 bg-gray-500 bg-opacity-60 flex justify-center items-{position} {bg_class} z-{zindex}" on:pointerdown={closeOnOutclick} transition:fade={{duration: 200}}>
-    <div class="shadow-lg max-h-[70%] bg-yellow-200" transition:fade={{duration: 200}}>
+    <div class="shadow-lg max-h-[50%] bg-yellow-200" transition:fade={{duration: 200}}>
       <slot></slot>
     </div>
   </div>
