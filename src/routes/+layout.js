b88e8d55c5c9aebd79d5c8956d5570a36bad601a
Attila <attila.ocsag@gmail.com>
2024-02-02 09:57:28 +0100

    navigator.virtualKeyboard put into layout.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,6 @@
 export const prerender = true;
 export const ssr = false;
+if ("virtualKeyboard" in navigator) {
+    console.log(navigator.virtualKeyboard.overlaysContent); // false
+    navigator.virtualKeyboard.overlaysContent = true; // Opt out of the automatic handling.
+  }
