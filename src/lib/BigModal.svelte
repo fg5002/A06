c48c42f3a429dcba1f9864afc26b1c41af6e01ae
Attila <attila.ocsag@gmail.com>
2024-02-02 22:06:07 +0100

    resizes-visual

--- src/lib/BigModal.svelte
+++ src/lib/BigModal.svelte
@@ -20,15 +20,15 @@
 
 <Modal bind:showModal={showBigModal} zindex=2000 bg_class={bgClass} position={position}>
   <div class="flex flex-col h-full gap-2 p-2 bg-lime-200 w-full border-zinc-500 border-2 rounded-md justify-items-center">
+    <div class="flex flex-col h-full w-full">
+      <slot/>
+    </div>
     <div class="w-full">
       <input 
       class="bg-yellow-200  focus:bg-yellow-300 border-2 border-zinc-500 rounded-md px-2 py-1 m-0 text-left text-lg w-[75%]" 
       type="text" 
       use:focus
       >
-    </div>
-    <div class="flex flex-col h-full w-full">
-      <slot/>
     </div>
   </div>
 </Modal>
