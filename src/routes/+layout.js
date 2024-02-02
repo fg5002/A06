2bb78dce623f7587ad351fc487b8226022faa3e2
Attila <attila.ocsag@gmail.com>
2024-02-02 12:52:49 +0100

    int widget text added to meta

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,2 @@
 export const prerender = true;
 export const ssr = false;
-/*if ("virtualKeyboard" in navigator) {
-  console.log(navigator.virtualKeyboard.overlaysContent); // false
-  navigator.virtualKeyboard.overlaysContent = true; // Opt out of the automatic handling.
-}*/
